c65118776dc0ed056937d8cbd74ae46c59f8341a
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:30:28 +0100

    Primera version

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+                int i,j,n;
+                System.out.print ("Input number of rows for the diamond: ");
+                Scanner in = new Scanner(System.in);
+                n = in.nextInt();
+
+                for(i=0;i<=n;i++)
+                {
+                    for(j=1;j<=n-i;j++)
+                        System.out.print(" ");
+                    for(j=1;j<=2*i-1;j++)
+                        System.out.print("*");
+                    System.out.print("\n");
+                }
+
+                for(i=n-1;i>=1;i--)
+                {
+                    for(j=1;j<=n-i;j++)
+                        System.out.print(" ");
+                    for(j=1;j<=2*i-1;j++)
+                        System.out.print("*");
+                    System.out.print("\n");
+                }
+
+    }
+}
+
+
+
+
